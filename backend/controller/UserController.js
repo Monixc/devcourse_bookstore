74146ba404584ba7eeed62496db20c3746087800
Monixc <chris13404@gmail.com>
2025-05-16 02:31:12 +0900

    feat: 로그인 기능 추가
    - UserController에 로그인 기능 구현
    - users.js에 로그인 및 비밀번호 재설정 라우터 추가
    - JWT를 사용한 인증 토큰 생성 및 쿠키 설정 추가

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,5 +1,9 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -15,4 +19,39 @@ const join = (req, res) => {
   });
 };
 
-module.exports = { join };
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "SELECT * FROM users WHERE email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    const loginUser = results[0];
+    if (loginUser && loginUser.password === password) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "5m", issuer: "monicx" }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+
+      console.log(token); //확인용
+      return res.status(StatusCodes.OK).json({ token });
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+
+const requrestReset = () => {};
+const passwordReset = () => {};
+
+module.exports = { join, login, requrestReset, passwordReset };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { join } = require("../controller/UserController");
+const {
+  join,
+  login,
+  requrestReset,
+  passwordReset,
+} = require("../controller/UserController");
 
 router.use(express.json());
 router.post("/join", join);
-
-router.post("/login", (req, res) => {
-  res.json({ message: "로그인" });
-});
-
-router.post("/reset", (req, res) => {
-  res.json({ message: "비밀번호 찾기" });
-});
-
-router.put("/reset", (req, res) => {
-  res.json({ message: "비밀번호 변경" });
-});
+router.post("/login", login);
+router.post("/reset", requrestReset);
+router.put("/reset", passwordReset);
 
 module.exports = router;
