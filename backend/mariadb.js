014de72b6e10d5c2f300b46d750ecb2d17a66a40
Monixc <chris13404@gmail.com>
2025-05-16 02:09:06 +0900

    feat: MariaDB 연결 설정 추가
    - MariaDB와의 연결을 위한 mariadb.js 파일 추가
    - 환경 변수로 데이터베이스 설정을 구성

---
+++ backend/mariadb.js
@@ -0,0 +1,11 @@
+const mariadb = require("mysql2");
+
+const connection = mariadb.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dateStrings: true,
+});
+
+module.exports = connection;
