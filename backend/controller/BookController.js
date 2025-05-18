178d9d27089e281c7df4bcae101041c0c070ba15
Monixc <chris13404@gmail.com>
2025-05-19 08:18:15 +0900

    feat: 도서 조회 API 구현
    - 전체 도서 조회 및 ID로 도서 조회 기능 추가
    - SQL 쿼리를 사용하여 데이터베이스에서 도서 정보를 가져오는 로직 구현

--- backend/controller/BookController.js
+++ backend/controller/BookController.js
@@ -2,10 +2,34 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllBooks = (req, res) => {
-  res.json({ message: "전체 도서 조회" });
+  let sql = "SELECT * FROM books";
+  conn.query(sql, (err, result) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    return res.status(StatusCodes.OK).json(result);
+  });
 };
 
-const getBookById = (req, res) => {};
+const getBookById = (req, res) => {
+  //parseInt 적용 시, id 속성을 구조 분해 시도
+  //parseInt는 숫자를 반환하므로, 구조 분해 불가. '
+  //parseInt는 문자열을 정수로 변환하는데, 객체인 req.parmas 전체를 전달하는 오류가 있었다.
+
+  let { id } = req.params;
+
+  let sql = "SELECT * FROM books WHERE id = ?";
+
+  conn.query(sql, id, (err, result) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    if (result[0]) return res.status(StatusCodes.OK).json(result[0]);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
+};
 
 const getBooksByCategory = (req, res) => {};
 
